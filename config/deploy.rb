baf1ea8fcace001b326c4041db484aafb76d4e1d
Josh Adams <josh@isotope11.com>
2008-06-21 17:22:26 -0500

    modified symlink for jabber login info

---
+++ config/deploy.rb
@@ -0,0 +1,68 @@
+default_run_options[:pty] = true
+
+set :application, "jabber_client"
+set :repository,  "git://github.com/knewter/jabber_client.git"
+
+set :deploy_to, "/home/deploy/#{application}"
+set :user,   "deploy"
+set :runner, "deploy"
+
+set :scm, :git
+
+role :app, "209.20.72.204"
+role :web, "209.20.72.204"
+role :db,  "209.20.72.204", :primary => true
+
+namespace :slicehost do
+  desc "Configure VHost"
+  task :config_vhost do
+    vhost_config =<<-EOF
+<VirtualHost *:8000>
+  ServerName libertyalabama.com
+  ServerAlias www.libertyalabama.com
+  DocumentRoot #{deploy_to}/current/public
+</VirtualHost>
+    EOF
+    put vhost_config, "src/vhost_config"
+    sudo "mv src/vhost_config /etc/apache2/sites-available/#{application}"
+    sudo "a2ensite #{application}"
+  end
+  
+  desc "Reload Apache"
+  task :apache_reload do
+    sudo "/etc/init.d/apache2 reload"
+  end
+
+  desc "install required gems"
+  task :install_required_gems do
+    run "cd #{deploy_to}/current; rake gems:install"
+  end
+
+  desc "git pull"
+  task :git_pull do
+    run "cd #{deploy_to}/current; git pull origin master"
+  end
+end
+
+desc "symlink database.yml"
+task :symlink_database_yml do
+  run "ln -s #{latest_release}/config/database.yml.production #{latest_release}/config/database.yml"
+end
+
+desc "update git submodules"
+task :update_submodules do
+  run "cd #{latest_release}; git submodule init; git submodule update;"
+end
+
+desc "Symlink jabber login info"
+task :symlink_jabber_login_info do
+  the_file = "jabber_client_test_login.rb"
+  run "ln -s #{latest_release}/shared/#{the_file} #{latest_release}/current/config/#{the_file}"
+end
+
+after "deploy:finalize_update" do
+  update_submodules
+  symlink_database_yml
+  symlink_jabber_login_info
+end
+
