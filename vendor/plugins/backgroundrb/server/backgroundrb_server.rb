16bebf214f92dfa9b5963419d968ec4227c5c673
Josh Adams <josh@isotope11.com>
2008-06-21 15:17:34 -0500

    removed backgroundrb as I'm not using it yet

--- vendor/plugins/backgroundrb/server/backgroundrb_server.rb
+++
@@ -1,9 +0,0 @@
-require "chronic"
-require "lib/bdrb_server_helper"
-require "lib/master_worker"
-require "lib/cron_trigger"
-require "lib/invalid_dump_error"
-require "lib/log_worker"
-require "lib/trigger"
-require "lib/meta_worker"
-
