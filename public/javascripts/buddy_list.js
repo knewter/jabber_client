8519d73645a2e2e68a0a8d0bbb310efef844d1e4
Josh Adams <josh@isotope11.com>
2008-06-22 05:40:38 -0500

    added back 'new message' link to buddy list

--- public/javascripts/buddy_list.js
+++ public/javascripts/buddy_list.js
@@ -22,12 +22,12 @@ var BuddyListUpdate = Class.create({
     }
   },
   /* <li class='status' id='user@server.com'>
-   *   <h3>UserName</h3>
+   *   <h3><a>UserName</a></h3>
    *   <span>message</span>
    * </li> */
   buddy_list_element_for: function(person){
     var li = Builder.node('li', { class: person[1], id: person[0] }, [
-               Builder.node('h3', person[0]),
+               Builder.node('h3', Builder.node('a', { href: '/messages/new?to=' + person[0] }, person[0])),
                Builder.node('span', person[2])
              ]);
     return li;
